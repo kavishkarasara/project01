1e060c22c13215ba8c5247e3c80607031ccd5543
Kavishka <kavishkarasara3@gmail.com>
2025-02-09 02:20:48 +0530

    update

--- src/Components/Upper/Upper.jsx
+++ src/Components/Upper/Upper.jsx
@@ -3,27 +3,29 @@ import React from "react";
 function Upper() {
   return (
     <div className="min-h-screen">
-      <section className="bg-gray-400 flex flex-col items-center justify-center py-8">
+      <section className="bg-gray-400 flex flex-col items-center justify-center">
         
         <section className="text-center px-4 lg:px-6 md:flex-row md:text-start md:gap-8">
           <div className="flex flex-col md:flex-row items-center">
-            <div className="max-w-xl p-4 md:p-6 pt-10 justify-center z-10 -mx-32">
-              <h1 className="text-3xl md:text-5xl lg:text-6xl font-bold">
-                Interior design
+          <div className="max-w-xl px-4 sm:px-6 md:px-8 py-10 flex flex-col items-start text-start z-10 ">
+              <h1 className="text-4xl sm:text-4xl md:text-5xl lg:text-6xl font-bold">
+              Interior Design
               </h1>
-              <p className="text-gray-700 text-base md:text-lg lg:text-xl mb-8 py-4 md:py-6">
+              <p className="text-gray-700 text-base sm:text-lg md:text-xl lg:text-2xl mb-6 sm:mb-8 py-4 sm:py-6">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Faucibus in libero risus semper habitant arcu eget. Et integer facilisi eget.
               </p>
-              <button className="bg-black text-white  md:py-3 px-6 md:px-8 text-lg rounded-full outline-none hover:bg-gray-800">
-                Explore
+              <button className="bg-black text-white py-2 sm:py-3 px-5 sm:px-8 text-lg rounded-full outline-none hover:bg-gray-800 transition duration-300">
+              Explore
               </button>
-            </div>
-            <div
-              className="h-[300px] w-full md:h-[400px] md:w-[400px] bg-center bg-cover md:ml-auto mt-8 md:mt-0 -mx-80"
-              style={{
-                backgroundImage: "url('/src/assets/image/NDT pic 2.jpg')",
-              }}
-            ></div>
+          </div>
+
+          <div 
+  className="w-full h-[250px] sm:h-[300px] md:h-[400px] md:w-[400px] bg-center bg-cover  mt-6 md:mt-0 -mx-40" 
+  style={{
+    backgroundImage: "url('/src/assets/image/NDT pic 2.jpg')",
+  }}
+></div>
+
           </div>
         </section>
         
@@ -34,7 +36,7 @@ function Upper() {
               backgroundImage: "url('/src/assets/image/NDT pic 1.jpg')",
             }}
           ></div>
-          <div className="bg-transparent md:bg-white/50 rounded-full p-4 md:p-6 w-full md:w-[700px] md:h-[250px] z-10 -mx-20">
+          <div className="bg-transparent md:bg-white/40 rounded-full p-4 md:p-6 w-full md:w-[700px] md:h-[250px] z-10 -mx-20">
             <div className="flex flex-col md:flex-row items-start md:items-center justify-center gap-6">
               <div className="w-full md:w-[150px] text-start p-2">
                 <h3 className="text-xl md:text-2xl font-bold">5.8</h3>
